e5bf33effabb883fb3e61f83e512648c1be6136a
Md-Rasel-Ahmed <nhd305812@gmail.com>
2026-04-07 21:32:42 +0500

    payment api done

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     // collection here
     const tasks = client.db("micro_task").collection("tasks");
     const users = client.db("micro_task").collection("users");
+    const withdrawCollection = client.db("micro_task").collection("withdraws");
     const submitedTaskCollection = client
       .db("micro_task")
       .collection("submitedTask");
@@ -41,7 +42,6 @@ async function run() {
       const filter = { email: email };
 
       const result = await tasks.find(filter).toArray();
-      console.log("buyer task", result);
       res.send(result);
     });
     app.post("/tasks", async (req, res) => {
@@ -87,22 +87,69 @@ async function run() {
       const updateDoc = {
         $set: {
           required_workers: findBuyerTask.required_workers - 1,
+          applied: findBuyerTask.applied + 1,
         },
       };
       const updateBuyerTask = await tasks.updateOne(buyerTaskFilter, updateDoc);
       res.send({ result, updateBuyerTask });
     });
+    app.get("/submitedTask/:email", async (req, res) => {
+      const query = { worker_email: req.params.email };
+      const result = await submitedTaskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/withdraw/:email", async (req, res) => {
+      console.log(req.params.email);
+      const query = { email: req.params.email };
+      const result = await withdrawCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/withdraw", async (req, res) => {
+      const data = req.body;
+      const filterUser = { email: data.email };
+      const findUser = await users.findOne(filterUser);
+      const updateUserCoins = {
+        $set: {
+          coins: findUser.coins - data.withdrawCoins,
+        },
+      };
+      const updateResult = await users.updateOne(filterUser, updateUserCoins);
+      const result = await withdrawCollection.insertOne(data);
+      res.send({ result, updateResult });
+    });
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const result = await users.findOne(filter);
-      console.log(result);
       res.send(result);
     });
     app.post("/users", async (req, res) => {
       const data = req.body;
       const result = await users.insertOne(data);
       res.send(result);
+    });
+    app.put("/users", async (req, res) => {
+      const task = req.body;
+      console.log(task);
+      const WorkerfindQuery = { email: task.worker_email };
+      const filterSubmitedTask = { _id: new ObjectId(task._id) };
+      const findWorker = await users.findOne(WorkerfindQuery);
+      const updateDoc = {
+        $set: {
+          coins: findWorker.coins + req.body.payable_amount,
+        },
+      };
+      const updateSubmittedTask = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await users.updateOne(WorkerfindQuery, updateDoc);
+      const result2 = await submitedTaskCollection.updateOne(
+        filterSubmitedTask,
+        updateSubmittedTask,
+      );
+      res.send({ result, result2 });
     });
 
     // Send a ping to confirm a successful connection
